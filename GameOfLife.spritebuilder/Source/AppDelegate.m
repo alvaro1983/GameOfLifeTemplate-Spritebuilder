2bae53fd28f6801f9ae381b102fc5d7f403d839c
Alvaro Ruiz <alvaro.ruiz@isobar.com>
2014-10-28 12:50:41 +0000

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/AppDelegate.m
+++ GameOfLife.spritebuilder/Source/AppDelegate.m
@@ -39,14 +39,20 @@
     NSMutableDictionary* cocos2dSetup = [NSMutableDictionary dictionaryWithContentsOfFile:configPath];
     
     // Note: this needs to happen before configureCCFileUtils is called, because we need apportable to correctly setup the screen scale factor.
-     
+#ifdef APPORTABLE
+    if([cocos2dSetup[CCSetupScreenMode] isEqual:CCScreenModeFixed])
+        [UIScreen mainScreen].currentMode = [UIScreenMode emulatedMode:UIScreenAspectFitEmulationMode];
+    else
+        [UIScreen mainScreen].currentMode = [UIScreenMode emulatedMode:UIScreenScaledAspectFitEmulationMode];
+#endif
+    
     // Configure CCFileUtils to work with SpriteBuilder
     [CCBReader configureCCFileUtils];
     
     // Do any extra configuration of Cocos2d here (the example line changes the pixel format for faster rendering, but with less colors)
     //[cocos2dSetup setObject:kEAGLColorFormatRGB565 forKey:CCConfigPixelFormat];
     
-    [self setupCocos2dWithOptions:cocos2dSetup];
+//    [self setupCocos2dWithOptions:cocos2dSetup];
     
     return YES;
 }
